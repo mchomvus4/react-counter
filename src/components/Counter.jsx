71c63e5e363f059dac575303d253fda16be45ff1
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-02-11 00:03:45 +0300

    rreac

---
+++ src/components/Counter.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import './Style.css'
+ class Counter extends Component {
+  constructor(props){
+   super(props)
+   this.state = {
+    count: 0
+   }
+  }
+  incrementCount = ()=>{
+    this.setState({
+     count:this.state.count +1
+    })
+    
+  }
+
+  decrementCount = ()=>{
+   this.setState({
+    count:this.state.count -1
+   })
+  }
+
+  resetCount = ()=>{
+   this.setState({
+    count:0
+   })
+  }
+  
+  
+  render() {
+  
+    return (
+      <div>
+   <main>
+          <div class="container">
+           <h3 class="counter">
+            react counter application
+           </h3>
+           <span id="value">{this.state.count}</span>
+           <div class="button-container">
+           
+            <button onClick ={this.decrementCount} class="btn decrease">decrease</button>
+            <button onClick ={this.resetCount} class="btn reset">reset</button>
+            <button onClick ={this.incrementCount} class="btn increase">increase</button>
+           </div>
+          </div>
+ </main>
+ <footer>
+  <p>
+   &copy;2021 Samwel Mchomvu From NOTTECH Lab
+  </p>
+ </footer>
+      </div>
+    )
+  }
+}
+
+export default Counter
